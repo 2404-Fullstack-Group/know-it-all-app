c18d61ae9249ecc30187d8f61f679c47c4607c53
Kai Fuller <kaifuller1995@gmail.com>
2024-09-11 21:49:09 -0600

    Token useState Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 // react imports
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { useState } from "react";
 
 // component imports
 import NavBar from "./components/sections/NavBar";
@@ -9,6 +10,7 @@ import RegistrationPage from "./components/pages/RegistrationPage";
 import CreateQuizPage from "./components/pages/CreateQuizPage";
 
 export default function App() {
+  const [token, setToken] = useState(null);
   return (
     <Router>
       <header>
@@ -18,7 +20,7 @@ export default function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/quiz-creator" element={<CreateQuizPage />} />
-          <Route path="/login" element={<LoginPage />} />
+          <Route path="/login" element={<LoginPage setToken={setToken} />} />
           <Route path="/register" element={<RegistrationPage />} />
         </Routes>
       </main>
