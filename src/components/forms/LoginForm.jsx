6264dc24287b13764f88e8efe8fa51dcba2a039b
Kai Fuller <kaifuller1995@gmail.com>
2024-09-15 17:01:21 -0600

    Login Form

--- src/components/forms/LoginForm.jsx
+++ src/components/forms/LoginForm.jsx
@@ -1,5 +1,5 @@
 // react imports
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import axios from "axios"
 
 
@@ -8,24 +8,28 @@ import Button from "../elements/Button";
 import Input from "../elements/Input";
 import Label from "../elements/Label";
 import Title from "../elements/Title";
+import { useState } from "react";
 
 export default function LoginForm({ setToken }) {
+  const [username, setUsername] = useState(null)
+  const [password, setPassword] = useState(null)
+  const navigate = useNavigate()
 
   const handleOnClick = async (e) => {
     e.preventDefault()
     const response = await axios.post("http://localhost:3000/api/users/login", {
-      username: "Admin",
-      password: "password"
+      username: username,
+      password: password
     })
-    console.log(response.data)
     setToken(response.data.token)
+    navigate("/")
   }
 
   return (
     <form className="login-form">
       <Title text="Login" />
-      <Input type="text" name="username" placeholder="username" />
-      <Input type="password" name="password" placeholder="password" />
+      <Input type="text" name="username" placeholder="username" onChange={(e) => {setUsername(e.target.value)}} />
+      <Input type="password" name="password" placeholder="password" onChange={(e) => {setPassword(e.target.value)}} />
       <Button text="login" onClick={handleOnClick}/>
       <Label
         text={
