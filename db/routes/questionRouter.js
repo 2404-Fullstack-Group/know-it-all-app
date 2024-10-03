453db27439a8f5445fa18d74a60cd02d9c1de5b3
Ryan Turaids <ryturaids@gmail.com>
2024-10-02 19:57:18 -0700

    updated questionRouter/random, added generateQuiz form and generateQuiz page

--- db/routes/questionRouter.js
+++ db/routes/questionRouter.js
@@ -30,4 +30,32 @@ questionRouter.get("/:question_id", async (req, res, next) => {
   }
 });
 
+// Get random questions based on category and difficulty
+questionRouter.get("/random", async (req, res, next) => {
+  try {
+    const { category, difficulty, questionCount } = req.body;
+
+    console.log(
+      `Received category: ${category}, difficulty: ${difficulty}, questionCount: ${questionCount}`
+    );
+
+    const questions = await prisma.question.findMany({
+      where: {
+        category: category,
+        difficulty: difficulty,
+      },
+    });
+
+    const shuffled = questions.sort(() => 0.5 - Math.random());
+    const selectedQuestions = shuffled.slice(0, parseInt(questionCount));
+
+    console.log(`Selected questions: ${JSON.stringify(selectedQuestions)}`);
+
+    res.send(selectedQuestions);
+  } catch (error) {
+    console.error("Error in /random route:", error);
+    res.status(500).json({ message: "Something went wrong." });
+  }
+});
+
 module.exports = questionRouter;
