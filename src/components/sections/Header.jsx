b5bef242c350d66cbac8022aac46e9895f5b43a5
Ryan Turaids <ryturaids@gmail.com>
2024-10-03 20:52:15 -0700

    fixed header

--- src/components/sections/Header.jsx
+++ src/components/sections/Header.jsx
@@ -18,29 +18,14 @@ export default function Header({
   };
 
   return (
-     <>
-      {(location.pathname !== "/" || !isHeader) ? 
-      <header className="site-header">
-        {isHeader ? (
-          <Link to="/">
-            <h1>
-              <JSXSpan text="Know It All" />
-            </h1>
-  </Link>
-        ) : null}  
-        <nav>
-          <Link to="/browse">
-            <JSXButton text="Browse" />
-          </Link>
-          <Link to="/quizzes">
-            <JSXButton text="Play" />
-          </Link>
-          <Link to="/create">
-            <JSXButton text="Create" />
-          </Link>
-          {token ? (
-            <Link to={`/profile/${userId}`}>
-              <JSXButton text="Profile" />
+    <>
+      {location.pathname !== "/" || !isHeader ? (
+        <header className="site-header">
+          {isHeader ? (
+            <Link to="/">
+              <h1>
+                <JSXSpan text="Know It All" />
+              </h1>
             </Link>
           ) : null}
           <nav>
