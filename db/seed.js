13946bd1439ef0c33f5f6b7e1b1f10a1e5ed4217
Kai Fuller <kaifuller1995@gmail.com>
2024-11-13 16:37:48 -0700

    Changed seed.js

--- db/seed.js
+++ db/seed.js
@@ -49,90 +49,87 @@ const categories = [
 ];
 
 const seed = async () => {
-  await prisma.q_junction.deleteMany({});
-  await prisma.quiz.deleteMany({});
-  await prisma.question.deleteMany({});
-  await prisma.user.deleteMany({});
-
-  // Creates the Admin user
-  const user = await prisma.user.create({
-    data: {
-      first_name: "Ryan",
-      last_name: "Fuller",
-      username: "Admin",
-      email: "admin@knowitall.com",
-      password: await bcrypt.hash("password", 5),
-      is_admin: true,
-    },
-  });
-
-  // inputs data from trivia-questions.json into the questions table
-  data.forEach(async (item) => {
-    await prisma.question.create({
+  if (prisma.user.findMany({}).length === 0) {
+    // Creates the Admin user
+    const user = await prisma.user.create({
       data: {
-        category: item.category,
-        tags: item.tags.length > 3 ? item.tags.slice(0, 3) : item.tags,
-        difficulty: item.difficulty,
-        question: item.question,
-        correctAnswer: item.correctAnswer,
-        incorrectAnswers: item.incorrectAnswers,
-        type: item.type,
-        user: {
-          connect: {
-            id: user.id,
-          },
-        },
+        first_name: "Ryan",
+        last_name: "Fuller",
+        username: "Admin",
+        email: "admin@knowitall.com",
+        password: await bcrypt.hash("password", 5),
+        is_admin: true,
       },
     });
-  });
 
-  // Creates a quiz for each category that includes 10 random questions
-  categories.forEach(async (category) => {
-    // Creates the category quiz
-    const quiz = await prisma.quiz.create({
-      data: {
-        user: {
-          connect: {
-            id: user.id,
-          },
-        },
-        category: category,
-      },
-    });
-    // find all questions from the questions table that is in a category
-    const categoryQuestions = await prisma.question.findMany({
-      where: {
-        category: category,
-      },
-    });
-    // List that will include non-repeating random numbers
-    const indexList = [];
-    // While loop to generate non-repeating random numbers and push to indexList
-    while (indexList.length < 10) {
-      const randomNum = Math.floor(Math.random() * categoryQuestions.length);
-      if (!indexList.includes(randomNum)) {
-        indexList.push(randomNum);
-      }
-    }
-    // Creates a q_junction entry for the quiz and questions
-    // q_junction allows for a many-to-many relationship between quizzes and questions
-    indexList.forEach(async (i) => {
-      await prisma.q_junction.create({
+    // inputs data from trivia-questions.json into the questions table
+    data.forEach(async (item) => {
+      await prisma.question.create({
         data: {
-          quiz: {
+          category: item.category,
+          tags: item.tags.length > 3 ? item.tags.slice(0, 3) : item.tags,
+          difficulty: item.difficulty,
+          question: item.question,
+          correctAnswer: item.correctAnswer,
+          incorrectAnswers: item.incorrectAnswers,
+          type: item.type,
+          user: {
             connect: {
-              id: quiz.id,
-            },
-          },
-          question: {
-            connect: {
-              id: categoryQuestions[i].id,
+              id: user.id,
             },
           },
         },
       });
     });
-  });
+
+    // Creates a quiz for each category that includes 10 random questions
+    categories.forEach(async (category) => {
+      // Creates the category quiz
+      const quiz = await prisma.quiz.create({
+        data: {
+          user: {
+            connect: {
+              id: user.id,
+            },
+          },
+          category: category,
+        },
+      });
+      // find all questions from the questions table that is in a category
+      const categoryQuestions = await prisma.question.findMany({
+        where: {
+          category: category,
+        },
+      });
+      // List that will include non-repeating random numbers
+      const indexList = [];
+      // While loop to generate non-repeating random numbers and push to indexList
+      while (indexList.length < 10) {
+        const randomNum = Math.floor(Math.random() * categoryQuestions.length);
+        if (!indexList.includes(randomNum)) {
+          indexList.push(randomNum);
+        }
+      }
+      // Creates a q_junction entry for the quiz and questions
+      // q_junction allows for a many-to-many relationship between quizzes and questions
+      indexList.forEach(async (i) => {
+        await prisma.q_junction.create({
+          data: {
+            quiz: {
+              connect: {
+                id: quiz.id,
+              },
+            },
+            question: {
+              connect: {
+                id: categoryQuestions[i].id,
+              },
+            },
+          },
+        });
+      });
+    });
+  }
 };
 
 seed();
